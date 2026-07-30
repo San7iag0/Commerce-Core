5586267d17362a95d036dbeb57b48918d177d13a
Santiago Bejarano <56372994+San7iag0@users.noreply.github.com>
2026-07-30 15:03:33 -0500

    health created successful

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 
 import { ConfigModule } from './infrastructure/config';
 import { PrismaModule } from './infrastructure/database/prisma';
+import { HealthModule } from './modules/health/health.module';
 
 @Module({
   imports: [
     ConfigModule,
     PrismaModule,
+    HealthModule
   ],
 })
 export class AppModule {}
